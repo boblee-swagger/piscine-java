fb7f19f687b3e31ff7cf39975795cc3f8619867d
kallgueye <gueyekallamoussa@gmail.com>
2025-03-12 04:53:11 +0000

    fix func name

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,5 @@
 public class StringConcat {
-    public static String concatStringBuilder(String s1, String s2) {
+    public static String concat(String s1, String s2) {
         StringBuilder sb = new StringBuilder();
         if (s1 != null) {
             sb.append(s1);
