ce1df818b21c8d79420ca8b0c7a14beeab328509
kallgueye <gueyekallamoussa@gmail.com>
2025-03-12 09:38:36 +0000

    fix null characters

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -1,41 +1,48 @@
 public class CleanExtract {
     public static String extract(String s) {
 
-        if (s.trim().length() != 0){
-             String[] substrings = s.split("\\|");
-            StringBuilder result = new StringBuilder(); 
+        String[] substrings = s.split("\\|");
+        StringBuilder result = new StringBuilder(); 
 
-            for (String sub : substrings) {
+        for (String sub : substrings) {
 
-                if (sub.startsWith(".") && sub.endsWith(".")) {
-                    int firstDot = sub.indexOf(".");
-                    int lastDot = sub.lastIndexOf(".");
-                    String str = sub.substring(firstDot , lastDot).trim();
-                    if (!str.isEmpty()) { 
-                        result.append(" ").append(str);
-                    }
-                } else if (sub.contains(".")) {
-                    int index = sub.indexOf(".");
-                    String str = sub.substring(index + 1).trim();
-                    if (!str.isEmpty()) { 
-                        result.append(" ").append(str);
-                    }
+            if (sub.startsWith(".") && sub.endsWith(".")) {
+                int firstDot = sub.indexOf(".");
+                int lastDot = sub.lastIndexOf(".");
+                String str = sub.substring(firstDot , lastDot).trim();
+                if (!str.isEmpty()) { 
+                    result.append(" ").append(str);
+                }
+            } else if (sub.contains(".")) {
+                int index = sub.indexOf(".");
+                String str = sub.substring(index + 1).trim();
+                if (!str.isEmpty()) { 
+                    result.append(" ").append(str);
                 }
             }
+        }
 
-            for (int i = 0; i < result.length()-1; i++) {
-                if (result.charAt(i) == '.') {
-                    result.deleteCharAt(i);
-                    i--; 
-                }
+        for (int i = 0; i < result.length()-1; i++) {
+            if (result.charAt(i) == '.') {
+                result.deleteCharAt(i);
+                i--; 
             }
+        }
 
-            String output = result.toString().replaceAll("\\s+", " ").trim();
-            return output;
-                
-            }else{
-                return "";
-            }
-    
+        String output = result.toString().replaceAll("\\s+", " ").trim();
+
+        if (output.equals(".")) {
+            return "";
+        }
+        return output;
+            
+            
+       
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CleanExtract.extract("The|. quick brown. | what do you ..| .fox .|. Jumps over the lazy dog. ."));
+        System.out.println(CleanExtract.extract("  | Who am .I  | .love coding,  |  |.  Coding is fun . | ...  "));
     }
 }
