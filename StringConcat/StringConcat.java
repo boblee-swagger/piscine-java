591f1772ca8d2c2ccee16b405459ea779d589e02
kallgueye <gueyekallamoussa@gmail.com>
2025-03-12 04:51:44 +0000

    stringbuilder for concat

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,12 @@
 public class StringConcat {
-    public static String concat(String s1, String s2) {
-        return s1.concat(s2);
+    public static String concatStringBuilder(String s1, String s2) {
+        StringBuilder sb = new StringBuilder();
+        if (s1 != null) {
+            sb.append(s1);
+        }
+        if (s2 != null) {
+            sb.append(s2);
+        }
+        return sb.toString();
     }
 }
