2b7c6ebde6e9b42726b414ef8e77c0ef4dcbc272
kallgueye <gueyekallamoussa@gmail.com>
2025-03-13 05:32:03 +0000

    newline error

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -1,50 +1,52 @@
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.FileReader;
+//import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
+//import java.io.PrintStream;
+import java.io.FileInputStream;
 
 public class Cat {
     //read content of a file an print it to stdout
     public static void cat(String[] args) throws IOException {
         //because the exercise says that you have to read the content of "a" file
-        if (args.length == 1) {
-            try(BufferedReader br = new BufferedReader(new FileReader(args[0]))){
-                StringBuilder sb = new StringBuilder();
-                String line = br.readLine();
-            
-
-                while(line != null){
-                    sb.append(line);
-                    sb.append(System.lineSeparator());
-                    line = br.readLine();
+        if (args.length == 1){
+            try (FileInputStream fileInput = new FileInputStream(args[0])) {
+                // Use a buffer for efficient reading
+                byte[] buffer = new byte[4096]; // 4KB buffer size is a reasonable choice
+                int bytesRead;
+                
+                // Read from file and write to stdout until end of file
+                while ((bytesRead = fileInput.read(buffer)) != -1) {
+                    System.out.write(buffer, 0, bytesRead);
                 }
-                System.out.print(sb.toString());
+                
+                // Flush to ensure all data is written
+                System.out.flush();
             }
+            
         }
+        
     }
 
     public static void main(String[] args) throws IOException {
-        PrintStream stdout = System.out;
+        // PrintStream stdout = System.out;
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(outputStream);
-        System.setOut(printStream);
+        // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        // PrintStream printStream = new PrintStream(outputStream);
+        // System.setOut(printStream);
 
-        cat(new String[]{"input.txt"});
-        String output = outputStream.toString();
-        // Reset out to stdout
-        System.setOut(stdout);
-        System.out.println(output.equals("test input file\n"));
+        // cat(new String[]{"input.txt"});
+        // String output = outputStream.toString();
+        // // Reset out to stdout
+        // System.setOut(stdout);
+        // System.out.println(output.equals("test input file\n"));
 
-        ByteArrayOutputStream outputStream2 = new ByteArrayOutputStream();
-        PrintStream printStream2 = new PrintStream(outputStream2);
-        System.setOut(printStream2);
+        // ByteArrayOutputStream outputStream2 = new ByteArrayOutputStream();
+        // PrintStream printStream2 = new PrintStream(outputStream2);
+        // System.setOut(printStream2);
 
-        cat(new String[]{});
-        String output2 = outputStream2.toString();
-        // Reset out to stdout
-        System.setOut(stdout);
-        System.out.println(output2.equals(""));
+        // cat(new String[]{});
+        // String output2 = outputStream2.toString();
+        // // Reset out to stdout
+        // System.setOut(stdout);
+        // System.out.println(output2.equals(""));
     }
 }
