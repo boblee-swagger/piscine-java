6fa394dbf7cdedfba4fed7ce0b743c8076272345
kallgueye <gueyekallamoussa@gmail.com>
2025-03-13 02:18:31 +0000

    fix devide by zero

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,17 +1,46 @@
+
+
 public class DoOp {
     public static String operate(String[] args) {
-        int leftOperand = Integer.parseInt(args[0]);
-        int rightOperand = Integer.parseInt(args[2]);
-        String operand = args[1];
-        int result;
-        switch (operand) {
-            case "+" -> result = leftOperand + rightOperand;
-            case "-" -> result = leftOperand - rightOperand;
-            case "*" -> result = leftOperand * rightOperand;
-            case "/" -> result = leftOperand / rightOperand;
-            default -> throw new AssertionError();
-        }
 
-        return Integer.toString(result);
+        if (args.length == 3) {
+            int leftOperand = Integer.parseInt(args[0]);
+            int rightOperand = Integer.parseInt(args[2]);
+            String operand = args[1];
+            int result = 0;
+
+            switch (operand) {
+                    case "+" -> result = leftOperand + rightOperand;
+                    case "-" -> result = leftOperand - rightOperand;
+                    case "*" -> result = leftOperand * rightOperand;
+                    case "%" -> {
+                        if (rightOperand != 0) {
+                            result = leftOperand % rightOperand;
+                        } else {
+                            System.out.println("Error\nit depends on the input.");
+                            System.exit(0);
+                        }
+                    }
+                    case "/" -> {
+                        if (rightOperand != 0) {
+                            result = leftOperand / rightOperand;
+                        } else {
+                            System.out.println("Error\nit depends on the input.");
+                            System.exit(0);
+                        }
+                    }
+                    default -> throw new AssertionError("Invalid operator: " + operand);
+            }
+            return Integer.toString(result);
+        }
+        System.out.println("Error\nit depends on the input.");
+        return "";   
+    }
+
+    public static void main(String[] args) {
+        System.out.println(operate(new String[]{"1","+","2"}));
+        System.out.println(operate(new String[]{"1","-","1"}));
+        System.out.println(operate(new String[]{"1","%","0"}));
+        System.out.println(operate(args));
     }
 }
