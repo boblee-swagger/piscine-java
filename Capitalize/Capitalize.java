9ee65ac8042cde85a3c8fbe9f77bcefd96a46f29
kallgueye <gueyekallamoussa@gmail.com>
2025-03-14 18:00:28 +0000

    file manager review

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -2,14 +2,14 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Capitalize {
-  public static void capitalize(String[] args) throws IOException {
+    public static void capitalize(String[] args) throws IOException {
         try ( 
              BufferedReader reader = new BufferedReader(new FileReader(args[0]));
              BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
              
         ) {
             
-           String line; 
+            String line; 
             while ((line = reader.readLine()) != null) {
                 String capitalizedLine = capitalizeWords(line);
                 writer.write(capitalizedLine);
@@ -18,7 +18,7 @@ public class Capitalize {
         }
     }
 
-     private static String capitalizeWords(String line) {
+    private static String capitalizeWords(String line) {
         StringTokenizer tokenizer = new StringTokenizer(line);
         StringBuilder capitalized = new StringBuilder();
 
@@ -26,7 +26,7 @@ public class Capitalize {
             String word = tokenizer.nextToken();
             if (word.length() > 0) {
                 capitalized.append(Character.toUpperCase(word.charAt(0)))
-                           .append(word.substring(1).toLowerCase()).append(" ");
+                    .append(word.substring(1).toLowerCase()).append(" ");
             }
         }
 
--- FileManager/FileManager.java
+++ FileManager/FileManager.java
@@ -1,12 +1,11 @@
-import java.io.IOException;
-import java.io.FileWriter;
-import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileManager {
     public static void createFile(String fileName, String content) throws IOException {
-
         
         try (FileWriter writer = new FileWriter(fileName)) {
             writer.write(content);
@@ -20,7 +19,6 @@ public class FileManager {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
                 fullString.append(line).append("\n");
             }
             return fullString.toString();
