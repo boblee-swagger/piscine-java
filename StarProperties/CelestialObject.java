f8589bf169a9a8d23fbc48fc5577897e20f51b29
kallgueye <gueyekallamoussa@gmail.com>
2025-03-15 02:48:55 +0000

    fix mismatch directory name

--- StarProperties/CelestialObject.java
+++ StarProperties/CelestialObject.java
@@ -6,17 +6,4 @@ public class CelestialObject {
     public double z;
     public String name;
 
-    public CelestialObject(){
-        x = 0.0;
-        y = 0.0;
-        z = 0.0;
-        name = "Soleil";
-    }
-
-    public CelestialObject (double x, double y, double z, String name){
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.name = name;
-    }
 }
