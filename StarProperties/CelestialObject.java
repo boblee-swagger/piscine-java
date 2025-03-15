807343466a638367e5c7372eafa7788f57b79aa0
kallgueye <gueyekallamoussa@gmail.com>
2025-03-15 02:45:31 +0000

    add on constructor

--- StarProperties/CelestialObject.java
+++ StarProperties/CelestialObject.java
@@ -1,8 +1,22 @@
+
+
 public class CelestialObject {
     public double x;
     public double y;
     public double z;
     public String name;
 
-    
+    public CelestialObject(){
+        x = 0.0;
+        y = 0.0;
+        z = 0.0;
+        name = "Soleil";
+    }
+
+    public CelestialObject (double x, double y, double z, String name){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
 }
