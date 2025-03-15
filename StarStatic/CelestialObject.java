19132884399f3436349663f07007e3dac650d7b7
kallgueye <gueyekallamoussa@gmail.com>
2025-03-15 04:36:04 +0000

    add methods

---
+++ StarStatic/CelestialObject.java
@@ -0,0 +1,63 @@
+public class CelestialObject {
+    private double x;
+    private double y;
+    private double z;
+    private String name;
+    public static final double KM_IN_ONE_AU = 150000000;
+
+    public CelestialObject(){
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject ( String name,double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX(){
+        return this.x;
+    }
+
+    public void setX(double x){
+        this.x = x;
+    }
+
+    public double getY(){
+        return this.y;
+    }
+
+    public void setY(double y){
+        this.y = y;
+    }
+
+    public double getZ(){
+        return this.z;
+    }
+
+    public void setZ(double z){
+        this.z = z;
+    }
+
+
+     public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public static double getDistanceBetween(CelestialObject ob1, CelestialObject ob2){
+        return (ob1.x + ob2.x) + (ob1.y + ob2.y);
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject ob1, CelestialObject ob2){
+        return ((ob1.x + ob2.x) + (ob1.y + ob2.y)) / CelestialObject.KM_IN_ONE_AU ;
+
+    }
+}
