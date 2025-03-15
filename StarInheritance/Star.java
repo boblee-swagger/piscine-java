e82c9e6afdb49d16203aeec5109c59dfb0a3ed90
kallgueye <gueyekallamoussa@gmail.com>
2025-03-15 08:35:07 +0000

    start with inheritance

---
+++ StarInheritance/Star.java
@@ -0,0 +1,13 @@
+public class Star extends CelestialObject{
+    private double magnitude;
+
+    public double getMagnitude(){
+        return this.magnitude;
+    }
+
+    public void setMagnitude(double x){
+        this.magnitude = x;
+    }
+
+
+}
