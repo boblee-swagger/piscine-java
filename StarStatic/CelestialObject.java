533fb296ca961bc4969625ae0252d42895790d47
kallgueye <gueyekallamoussa@gmail.com>
2025-03-15 04:52:20 +0000

    correct result convertion

--- StarStatic/CelestialObject.java
+++ StarStatic/CelestialObject.java
@@ -53,11 +53,20 @@ public class CelestialObject {
     }
 
     public static double getDistanceBetween(CelestialObject ob1, CelestialObject ob2){
-        return (ob1.x + ob2.x) + (ob1.y + ob2.y);
+        return (ob1.getX() + ob2.getX()) + (ob1.getY() + ob2.getY());
     }
 
     public static double getDistanceBetweenInKm(CelestialObject ob1, CelestialObject ob2){
-        return ((ob1.getX() + ob2.getX()) + (ob1.getY() + ob2.getY())) / CelestialObject.KM_IN_ONE_AU ;
+        return (getDistanceBetween(ob1, ob2)) *  CelestialObject.KM_IN_ONE_AU;
 
+    }
+
+     public static void main(String[] args) {
+        CelestialObject defaultStar = new CelestialObject();
+        CelestialObject earth = new CelestialObject("Terre", 1.0, 2.0, 2.0);
+
+        System.out.println(CelestialObject.getDistanceBetween(defaultStar, earth));
+        System.out.println(CelestialObject.getDistanceBetweenInKm(defaultStar, earth));
+        System.out.println(CelestialObject.KM_IN_ONE_AU);
     }
 }
