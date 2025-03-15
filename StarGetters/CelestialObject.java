781300166eda571f121fd8eb044f8284b8126c50
kallgueye <gueyekallamoussa@gmail.com>
2025-03-15 03:14:58 +0000

    Start getters

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,44 @@
+public class CelestialObject {
+    private double x;
+    private double y;
+    private double z;
+    private String name;
+
+    public CelestialObject(){
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject ( String name,double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX(){
+        return this.x;
+    }
+
+    public void setX(double x){
+        this.x = x;
+    }
+
+    public double getY(){
+        return this.y;
+    }
+
+    public void setY(double y){
+        this.y = y;
+    }
+
+     public String getName(){
+        return this.name;
+    }
+
+    public void setX(String name){
+        this.name = name;
+    }
+}
