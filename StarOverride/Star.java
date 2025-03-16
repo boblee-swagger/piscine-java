c9d313353ef7c4b121d04e298c944e455370d636
kallgueye <gueyekallamoussa@gmail.com>
2025-03-16 05:43:14 +0000

    fix objects comparaison

--- StarOverride/Star.java
+++ StarOverride/Star.java
@@ -1,34 +1,90 @@
 
+// import java.util.Objects;
+
+
+// public class Star extends CelestialObject{
+//     private double magnitude;
+
+
+//     public Star(){
+//         this.magnitude = 0.0;
+//     }
+
+//     public Star(String name, double x, double y, double z, double magnitude){
+//         super(name, x, y, z);
+//         this.magnitude = magnitude;
+//     }
+
+//     public double getMagnitude(){
+//         return this.magnitude;
+//     }
+
+//     public void setMagnitude(double x){
+//         this.magnitude = x;
+//     }
+
+//      @Override
+//     public int hashCode() {
+//         return Objects.hash(super.hashCode(), magnitude);
+//     }
+
+ 
+//     @Override
+//     public boolean equals(Object obj) {
+//         if (this == obj)
+//             return true;
+//         if (obj == null || getClass() != obj.getClass())
+//             return false;
+//         if (!super.equals(obj))
+//             return false;
+
+//         Star star = (Star) obj;
+//         return Double.compare(this.magnitude, star.magnitude) == 0;
+
+//     }
+
+   
+//     @Override
+//     public String toString() {
+//         return String.format("%s shines at the %.3f magnitude", getName(), magnitude);
+//     }
+
+//       public static void main(String[] args) {
+//         Star star = new Star();
+//         Star star2 = new Star();
+//         Star proxima = new Star("Proxima", 18.389, 832.32, 218, 0.4);
+        
+//         System.out.println(star.toString());
+//         System.out.println(proxima.toString());
+//         System.out.println(star.equals(star2));
+//         System.out.println(star.equals(proxima));
+//     }
+// }
+
+
+
 import java.util.Objects;
 
-
-public class Star extends CelestialObject{
+public class Star extends CelestialObject {
     private double magnitude;
 
-
-    public Star(){
+    public Star() {
         this.magnitude = 0.0;
     }
 
-    public Star(String name, double x, double y, double z, double magnitude){
+    public Star(String name, double x, double y, double z, double magnitude) {
         super(name, x, y, z);
         this.magnitude = magnitude;
     }
 
-    public double getMagnitude(){
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public double getMagnitude() {
         return this.magnitude;
     }
 
-    public void setMagnitude(double x){
-        this.magnitude = x;
-    }
-
-     @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), magnitude);
-    }
-
- 
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -40,23 +96,15 @@ public class Star extends CelestialObject{
 
         Star star = (Star) obj;
         return Double.compare(this.magnitude, star.magnitude) == 0;
-
     }
 
-   
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), magnitude);
+    }
+
     @Override
     public String toString() {
         return String.format("%s shines at the %.3f magnitude", getName(), magnitude);
-    }
-
-      public static void main(String[] args) {
-        Star star = new Star();
-        Star star2 = new Star();
-        Star proxima = new Star("Proxima", 18.389, 832.32, 218, 0.4);
-        
-        System.out.println(star.toString());
-        System.out.println(proxima.toString());
-        System.out.println(star.equals(star2));
-        System.out.println(star.equals(proxima));
     }
 }
