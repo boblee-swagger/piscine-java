f6e3935e1189788d09e120a9aaf77a7147a17f3d
kallgueye <gueyekallamoussa@gmail.com>
2025-03-16 09:07:13 +0000

    star planet

---
+++ StarPlanet/Planet.java
@@ -0,0 +1,51 @@
+
+import java.util.Objects;
+
+public class Planet extends CelestialObject {
+    private Star centerStar;
+
+    public Planet(){
+        super();
+        this.centerStar = new Star();
+    }
+
+    public Planet(String name, double x, double y,  double z, Star centerStar){
+        super(name, x, y, z);
+        this.centerStar = centerStar;
+    }
+
+    public Star getCenterStar(){
+        return this.centerStar;
+    }
+
+    public void setCenterStar(Star centerStar){
+        this.centerStar = centerStar;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(this.centerStar.hashCode(), this.centerStar.getZ());
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj != null){
+            Planet planet = (Planet) obj;
+            return this.hashCode() == planet.hashCode();
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s circles around %s at the _ AU", this.getName(), this.centerStar.getName());
+    }
+
+    public double distanceWithCenters(Planet planet) {
+        return Planet.getDistanceBetween(planet, planet.centerStar);
+    }
+
+
+
+
+}
