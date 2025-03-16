c67dfa9747352d1dc41fc456236b8c31d7664f0d
kallgueye <gueyekallamoussa@gmail.com>
2025-03-16 10:19:25 +0000

    intro to the List

---
+++ StarGalaxy/Galaxy.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Galaxy extends CelestialObject {
+    private List<CelestialObject> celestialObject;
+
+    public Galaxy(){
+        this.celestialObject = new ArrayList();
+    }
+
+    public List<CelestialObject> getCelestialObjects(){
+        return this.celestialObject;
+    }
+
+    public void addCelestialObject(CelestialObject obj){
+        this.celestialObject.add(obj);
+    }
+
+}
---
+++ StarGalaxy/Star.java
@@ -0,0 +1,46 @@
+
+import java.util.Objects;
+
+
+public class Star extends CelestialObject{
+    private double magnitude;
+
+
+    public Star(){
+        this.magnitude = 0.0;
+    }
+
+    public Star(String name, double x, double y, double z, double magnitude){
+        super(name, x, y, z);
+        this.magnitude = magnitude;
+    }
+
+    public double getMagnitude(){
+        return this.magnitude;
+    }
+
+    public void setMagnitude(double x){
+        this.magnitude = x;
+    }
+
+     @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), this.magnitude);
+    }
+
+ 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj != null){
+            Star star = (Star) obj;
+            return this.hashCode() == star.hashCode();
+        }
+        return false;
+    }
+
+   
+    @Override
+    public String toString() {
+        return String.format("%s shines at the %.3f magnitude", getName(), magnitude);
+    }
+}
