883106af0034a1fb512d062ab97da84541bc4c4d
kallgueye <gueyekallamoussa@gmail.com>
2025-03-18 02:17:43 +0000

    adventure character

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,41 @@
+public class Character {
+    private final int maxHealth = 0;
+    private int currentHealth;
+    private final String name = null;
+
+    public Character(String name, int maxHealth){
+        this.currentHealth = maxHealth;
+    }
+
+    public int getMaxHealth(){
+        return this.maxHealth;
+    }
+
+     public int getCurrentHealth(){
+        return this.currentHealth;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    @Override
+    public String toString(){
+        if (this.currentHealth == 0){
+            return String.format("%s : KO", this.name);
+        }
+        return String.format("%s : %d/%d", this.name, this.currentHealth, this.maxHealth);
+    }
+
+    public void takeDamage(int n){
+        if (n > 0){
+            this.currentHealth -= n;
+        }
+    }
+
+    public void attack(Character player){
+        this.takeDamage(9);
+    }
+
+
+}
