d448f9f664e0cb6c4d362d3e5b43eb69f89d8fed
kallgueye <gueyekallamoussa@gmail.com>
2025-03-19 07:09:27 +0000

    adventure utils

--- AdventureUtils/Character.java
+++ AdventureUtils/Character.java
@@ -7,75 +7,73 @@ public class Character {
     private final String name;
     private static List<Character> allCharacters = new ArrayList<>();
 
-    public Character(String name, int maxHealth) {
+    public Character(String name, int maxHealth){
         this.name = name;
         this.maxHealth = maxHealth;
         this.currentHealth = maxHealth;
         allCharacters.add(this);
     }
 
-    public int getMaxHealth() {
+    public int getMaxHealth(){
         return this.maxHealth;
     }
 
-    public void resetHealth() {
-        this.currentHealth = this.maxHealth;
-    }
-
-    public int getCurrentHealth() {
+    public int getCurrentHealth(){
         return this.currentHealth;
     }
 
-    public String getName() {
+    public String getName(){
         return this.name;
     }
 
-    public String toString() {
-        return this.currentHealth == 0 ? String.format("%s : KO", this.name)
-                : String.format("%s : %d/%d", this.name, this.currentHealth, this.maxHealth);
+    @Override
+    public String toString(){
+        if (this.currentHealth == 0){
+            return String.format("%s : KO", this.name);
+        }
+        return String.format("%s : %d/%d", this.name, this.currentHealth, this.maxHealth);
     }
 
-    public void takeDamage(int damage) {
-        if (this.currentHealth - damage < 0) {
+    public void takeDamage(int n){
+        if (this.currentHealth - n > 0){
+            this.currentHealth -= n;
+        }else{
             this.currentHealth = 0;
-        } else {
-            this.currentHealth -= damage;
         }
     }
 
-    public void attack(Character character) {
-        character.takeDamage(9);
+    public static void attack(Character player){
+        player.takeDamage(9);
     }
 
-    public static String printStatus() {
-        if (allCharacters.size() == 0) {
-            return """
-                    ------------------------------------------
-                    Nobody's fighting right now !
-                    ------------------------------------------
-                    """;
-        } else {
-            StringBuilder result = new StringBuilder();
-            result.append("------------------------------------------\n");
-            result.append("Characters currently fighting :\n");
-            for (Character character : allCharacters) {
-                result.append(" - ").append(character.toString()).append("\n");
+    public static String printStatus(){
+        StringBuilder output = new StringBuilder();
+        if (!Character.allCharacters.isEmpty()){
+            output.append("------------------------------------------\n");
+            output.append("Characters currently fighting :\n");
+            for (Character character : Character.allCharacters) {
+                output.append(String.format(" - %s\n", character.toString()));
             }
-            result.append("------------------------------------------\n");
-
-            return result.toString();
+            output.append("------------------------------------------");
+        }else{
+            output.append("------------------------------------------\n");
+            output.append("Nobody's fighting right now !\n");
+            output.append("------------------------------------------");
         }
+        return output.toString();
     }
 
-    public static Character fight(Character char1, Character char2) {
-        while (char1.getCurrentHealth() > 0 && char2.getCurrentHealth() > 0) {
-            char1.attack(char2);
-            if (char2.getCurrentHealth() <= 0) {
-                return char1;
+    public static Character fight(Character player1, Character player2){
+
+        while(player2.currentHealth > 0 && player1.currentHealth > 0){
+            Character.attack(player2);
+            if (player2.currentHealth <= 0){
+                return player1;
             }
-            char2.attack(char1);
-            if (char1.getCurrentHealth() <= 0) {
-                return char2;
+
+            Character.attack(player1);
+            if (player1.currentHealth <= 0){
+                return player2;
             }
         }
         return null;
