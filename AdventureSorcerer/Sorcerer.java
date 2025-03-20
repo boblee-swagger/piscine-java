1bcc1c7174b605f1950ae60a5427687fc5262ce3
kallgueye <gueyekallamoussa@gmail.com>
2025-03-20 07:21:20 +0000

    deep on implementations

---
+++ AdventureSorcerer/Sorcerer.java
@@ -0,0 +1,33 @@
+public class Sorcerer extends Character implements Healer {
+    
+    private int healCapacity;
+    public Sorcerer(String name, int maxHealth, int healCapacity){
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+    }
+
+    @Override
+    public int getHealCapacity(){
+        return this.healCapacity;
+    }
+
+    @Override
+    public void heal(Character character){
+        if (this.healCapacity < this.getMaxHealth()){
+            character.currentHealth += this.healCapacity;
+        }else{
+            character.currentHealth = this.getMaxHealth();
+        }
+    }
+
+    @Override
+    public String toString(){
+        if (this.currentHealth <= 0){
+            return String.format("%s is a sorcerer with %d HP. It can heal %d HP.", this.getName(), this.getCurrentHealth(), this.healCapacity);
+        }
+        return String.format("%s is a dead sorcerer. So bad, it could heal %d HP.", this.getName(), this.healCapacity);
+    }
+
+
+
+}
