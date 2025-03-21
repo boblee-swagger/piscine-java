750a67ec479147d58e80078fa776a8bf8bd6faf5
kallgueye <gueyekallamoussa@gmail.com>
2025-03-21 02:57:30 +0000

    into the Lists

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -5,8 +5,6 @@ public class ListEquals{
 
         if (list1 != null && list2 != null){
             return list1.equals(list2);
-        }else if (list1.isEmpty() && list2.isEmpty()){
-            return false;
         }
 
         return false;
