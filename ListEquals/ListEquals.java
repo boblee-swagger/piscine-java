e58d2329e6e53a8a4168800d081339b329dad3f2
kallgueye <gueyekallamoussa@gmail.com>
2025-03-21 02:49:26 +0000

    into the Lists

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,6 +2,10 @@ import java.util.List;
 
 public class ListEquals{
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-       return list1.equals(list2);
+
+        if (list1 != null && list2 != null){
+            return list1.equals(list2);
+        }
+        return false;
     }
 }
